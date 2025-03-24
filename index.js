1017b8324e8d4bd18c49bb8ddd4eeafae429019a
Shujaat Ahmed <ahmed.shujaat555@gmail.com>
2025-03-25 03:49:59 +0500

    packages added

--- index.js
+++ index.js
@@ -3,18 +3,17 @@ import cors from "cors";
 import fetch from "node-fetch";
 import dotenv from "dotenv";
 
-dotenv.config(); // Load environment variables
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(express.json()); // Enable JSON parsing
+app.use(express.json());
 
 const SHOPIFY_API_URL =
   "https://your-store-name.myshopify.com/api/2024-01/graphql.json";
 
-// Shopify Access Token (Store in .env file)
 const ACCESS_TOKEN = process.env.SHOPIFY_ACCESS_TOKEN;
 
 app.post("/add-to-cart", async (req, res) => {
